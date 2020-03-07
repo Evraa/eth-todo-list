aebec3f214e580005a70e33d5fc70cefae45c7cd
Evraa <evramyousef@gmail.com>
2020-03-07 17:31:46 +0200

    List Tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,6 +1,24 @@
 pragma solidity ^0.5.0;
 
 contract TodoList{
-
     uint public taskCount =0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+    // uint -> ket, Task is the value
+    //will be like our database
+    mapping (uint => Task) public tasks;
+
+    constructor () public {
+        createTask("Wake up");
+    }
+
+    function createTask(string memory _content) public{
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount,_content,false);
+    }
+
 }
